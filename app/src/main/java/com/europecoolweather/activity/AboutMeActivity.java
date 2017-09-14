29c1236ed524137c3ae4acdb9511c2565723cf48
GuangKai <dengyuqust@163.com>
2017-09-14 15:35:21 +0800

    [20170913]代码优化

--- app/src/main/java/com/europecoolweather/activity/AboutMeActivity.java
+++ app/src/main/java/com/europecoolweather/activity/AboutMeActivity.java
@@ -1,7 +1,11 @@
 package com.europecoolweather.activity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.europecoolweather.R;
 import com.europecoolweather.util.DebugLog;
@@ -9,6 +13,7 @@ import com.europecoolweather.util.DebugLog;
 public class AboutMeActivity extends AppCompatActivity {
 
     private static final String TAG = "AboutMeActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +24,16 @@ public class AboutMeActivity extends AppCompatActivity {
 
     private void initView()
     {
+        Button mButton = (Button)findViewById(R.id.btn_search_author_blog);
+
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("http://blog.csdn.net/xiaoxiangyuhai?viewmode=list");
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
         DebugLog.d(TAG,"initView() complete");
     }
 }
